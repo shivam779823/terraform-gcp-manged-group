e2b686bc5c8f708e73ab32115ec8e55e36c1209d
shivam <shivam@gmail.com>
2022-08-20 00:28:12 -0400

    kdkd

--- main.tf
+++ main.tf
@@ -72,16 +72,3 @@ resource "google_compute_instance_group_manager" "default" {
   base_instance_name = "vm"
   target_size        = 2
 }
-
-# allow access from health check ranges
-resource "google_compute_firewall" "default" {
-  name          = "lb-fw-allow-hc"
-  provider      = google-beta
-  direction     = "INGRESS"
-  network       = google_compute_network.default.id
-  source_ranges = ["130.211.0.0/22", "35.191.0.0/16"]
-  allow {
-    protocol = "tcp"
-  }
-  target_tags = ["allow-health-check","vm1"]
-}
