91998a96677fadba9fc1e3f5c82836590f76005a
shivam <shivam@gmail.com>
2022-09-28 04:48:00 -0400

    update

--- backend.tf
+++ backend.tf
@@ -1,26 +1,9 @@
-resource "random_id" "bucket_prefix" {
-  byte_length = 5
-  
-}
 
-resource "google_storage_bucket" "test-admin" {
-  name = "${random_id.bucket_prefix.hex}-bucket-tfstate"
-  storage_class = "standard"
-  location = "US"
-
-  lifecycle {
-    prevent_destroy = true
-  }
-
-  versioning {
-    enabled = true
-  }
-}
 
 
 terraform {
  backend "gcs" {
-   bucket  = "${random_id.bucket_prefix.hex}-bucket-tfstate"
+   bucket  = "sm-test-p"
    prefix  = "terraform/state"
  }
 }
--- provider.tf
+++ provider.tf
@@ -1,3 +1,15 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "3.5.0"
+    }
+  }
+}
+
+
+
+
 provider "google" {
   region = var.region
   project = var.project
